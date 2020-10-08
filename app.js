67b30131a18b4ff883d6bbe06af213090ed8a265
salamidrus <idrussalam95@gmail.com>
2020-10-08 08:35:18 +0700

    Initial Setup & create user module

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// env variables
+require("dotenv").config();
+
+const app = express();
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// db config
+const mongoURI = process.env.MONGO_URI;
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Connected to MongoDB"));
+
+// routes
+// const userRoutes = require("./routes/user");
+
+// app.use("/api/v1/user", userRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Listening to ${PORT}`));
---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    match: [
+      /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+      "Please input in email format, example: asmith@mactec.com or foo12@foo.edu or bob.smith@foo.tv ",
+    ],
+    lowercase: true,
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    trim: true,
+  },
+  mobileNumber: {
+    type: String,
+    match: [
+      /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/,
+      "Please input in mobile number format, example:  (+44)(0)20-12341234 or 02012341234 or +44 (0) 1234-1234 ",
+    ],
+  },
+  profilePhoto: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/drovood07/image/upload/v1574847777/uploads/doctor_xp3dmi.png",
+  },
+});
